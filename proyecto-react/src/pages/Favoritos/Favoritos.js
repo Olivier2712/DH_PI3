72574e12af0a1ea9d70bdb82c8dd1b9d3803d8fd
mati-quintana-udesa <quintanam@udesa.edu.ar>
2022-09-18 12:59:16 +0200

    borra en favoritos pero si recargas vuelve

--- proyecto-react/src/pages/Favoritos/Favoritos.js
+++ proyecto-react/src/pages/Favoritos/Favoritos.js
@@ -13,6 +13,13 @@ class Favoritos extends Component {
         this.setState({favoritos: JSON.parse(localStorage.getItem('favoritos'))})
     }
 
+    borrarFav(id) {
+      const resto = this.state.favoritos.filter(contenido => contenido.id !== id)
+      this.setState({
+        favoritos: resto
+      })
+    }
+
   render() {
     return (
       <div className='card-container' style={{display:"flex", flexDirection:"row"}}>
@@ -20,6 +27,7 @@ class Favoritos extends Component {
             <Card 
                 key={item.id}
                 contenido={item}
+                borrar={(contenidoBorrar) => this.borrarFav(contenidoBorrar)}
             />
         ))}
       </div>
