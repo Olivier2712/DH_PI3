46052aa7e5769f47595aabffa02ebaeb01e83d21
tomicampastro <tomycampastrogallo@gmail.com>
2022-09-17 23:35:11 -0300

    css  detalle

--- proyecto-react/src/pages/Detalles/Detalle.jsx
+++ proyecto-react/src/pages/Detalles/Detalle.jsx
@@ -57,25 +57,14 @@ class Detalle extends React.Component{
 
     render(){
         return(
-<<<<<<< HEAD
-            <div className="detalle">
-                <h1 className='nombre-detalle'>{this.state.pelicula.titulo}</h1>
-                <img className="img-detalle" src={this.state.pelicula.foto}  /> 
-                <h2 className='calificacion-detalle' >Calificacion: {this.state.pelicula.califiacion}</h2>
-                <h2 className='estreno-detalle' >Fecha de estreno: {this.state.pelicula.fechaDeEstreno}</h2>
-                <h2 className='duracion-detalle' >Duracion: {this.state.pelicula.duracion}</h2>
-                <h2 className='sinapsis-titulo'>Sinapsis:</h2> <h2 className='sinapsis-detalle' >{this.state.pelicula.sinapsis}</h2>
-                <h2 className='duracion-detalle' >Genero: {this.state.pelicula.genero}</h2>
-=======
-            <div>
-                <h1>{this.state.contenido.titulo}</h1>
-                <img src={this.state.contenido.foto} height={136} width={136} /> 
-                <h2>Calificacion: {this.state.contenido.califiacion}</h2>
-                <h2>Fecha de estreno: {this.state.contenido.fechaDeEstreno}</h2>
-                <h2>Duracion: {this.state.contenido.duracion}</h2>
-                <h2>Sinapsis: {this.state.contenido.sinapsis}</h2>
-                <h2>Genero: {this.state.contenido.genero}</h2>
->>>>>>> a5ac1066ddd11e9c1703fe96d7c610a4f3854ecb
+            <div className='detalle' >
+                <h1 className='nombre-detalle' >{this.state.contenido.titulo}</h1>
+                <img className='img-detalle' src={this.state.contenido.foto} height={136} width={136} /> 
+                <h2 className='calificacion-detalle' >Calificacion: {this.state.contenido.califiacion}</h2>
+                <h2 className='estreno-detalle' >Fecha de estreno: {this.state.contenido.fechaDeEstreno}</h2>
+                <h2 className='duracion-detalle' >Duracion: {this.state.contenido.duracion}</h2>
+                <h2 className='sinapsis-titulo'> Sinapsis:</h2> <h2 className='sinapsis-detalle' > {this.state.contenido.sinapsis}</h2>
+                <h2 className='genero-detalle' >Genero: {this.state.contenido.genero}</h2>
             </div>
         )
 
