64ff68e31f3258363d277b02f19349b2191644a8
Olivier2712 <olegras@udesa.edu.ar>
2022-09-18 14:39:08 -0300

    Descripcion oculta del trabajo

--- proyecto-react/src/components/Card/Card.js
+++ proyecto-react/src/components/Card/Card.js
@@ -3,33 +3,49 @@ import "./card.css";
 import { Link, Redirect, useHistory } from 'react-router-dom'
 import { getMouseEventOptions } from '@testing-library/user-event/dist/utils';
 import { converterCategoriaId } from '../../pages/Detalles/categoriaIdTool';
+import { Collapse } from 'react-bootstrap';
+import AccordionBody from 'react-bootstrap/esm/AccordionBody';
+
 
 
 
 function Card(props) {
-  let { name, title, id, poster_path } = props.contenido
-  const categoria= props.categoria
+  let { name, title, id, poster_path, overview } = props.contenido
+  const categoria = props.categoria
 
   const history = useHistory()
 
   const vermas = () => {
     //console.log("vermas")
   }
-  function handleOnClick(){
+  function handleOnClick() {
     const categoriaId = converterCategoriaId(categoria, id)
-    history.push("/contenidos/detalle/"+categoriaId)
+    history.push("/contenidos/detalle/" + categoriaId)
   }
 
   return (
     <div className='movie-card'>
-      <img className="img_card"src={`https://image.tmdb.org/t/p/original${poster_path}`} alt={name} />
+      <img className="img_card" src={`https://image.tmdb.org/t/p/original${poster_path}`} alt={name} />
       <h4 className='nombre'>{name} {title}</h4>
 
       <div className='botones'>
         <button className="btn_fav" onClick={() => { props.favorito(props.contenido) }} >Favoritos</button>
         <button className="btn_borrar" onClick={() => props.borrar(id)} >Borrar</button>
-        <button className="btn_vermas" onClick={vermas}>Ver Mas</button>
-        <button onClick={handleOnClick} className="btn_detalle" >Detalle</button>
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item" style={{marginRight:"10px"}}>
+            <h2 className="accordion-header" id="headingOne">
+              <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={{backgroundColor:"#54df0e", color:"white", fontSize:"large", fontWeight:"bold"}}>
+                Sinopsis
+              </button>
+            </h2>
+            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div className="accordion-body" style={{background:"black", color:"white", fontWeight:"bold"}}>
+              {overview}
+              </div>
+            </div>
+          </div>
+          <button onClick={handleOnClick} className="btn_detalle" >Detalle</button>
+        </div>
       </div>
     </div>
   )
